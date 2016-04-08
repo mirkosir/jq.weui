935c05ef5f5990412375aececa9c1a96a27625dd
jianqi <xhzkds@qq.com>
2016-04-09 00:18:10 +0800

    修复一处语法错误

--- jq.weui/jq.weui.js
+++ jq.weui/jq.weui.js
@@ -79,7 +79,7 @@
              
 
 
-            var html = '<div class="weui_mask" style="z-index:'+jq.level+'"></div>' + '<div class="weui_dialog" style="z-index:'+(jq.level+1)+'">' + '<div class="weui_dialog_hd" style="margin-top:10px;padding: 1.2em 20px .5em;"><strong class="weui_dialog_title" style="font-size:15px; color:#888; ">你确定要删除吗？</strong></div><div class="weui_dialog_ft">' + '    <a href="javascript:;" class="weui_btn_dialog primary alertbtn" id="alertbtn">确定</a>' + '</div>' + '</div>';
+            var html = '<div class="weui_mask" style="z-index:'+jq.level+'"></div>' + '<div class="weui_dialog" style="z-index:'+(jq.level+1)+'">' + '<div class="weui_dialog_hd" style="margin-top:10px;padding: 1.2em 20px .5em;"><strong class="weui_dialog_title" style="font-size:15px; color:#888; ">'+msg+'</strong></div><div class="weui_dialog_ft">' + '    <a href="javascript:;" class="weui_btn_dialog primary alertbtn" id="alertbtn">确定</a>' + '</div>' + '</div>';
 
             jq._addDom('dialog2', 'weui_dialog_alert', html);
 
